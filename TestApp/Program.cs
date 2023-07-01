94abbd1d0c41c21ccbd16d69caafab897cb5fb14
Marco Winter <marco.winter@enviaM.de>
2023-07-01 13:02:35 +0200

    added test program

---
+++ TestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Pipelines;
+using System.Linq;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var pipeline = new Pipeline()
+    .First(async () => new Success<string[]>(await File.ReadAllLinesAsync("data1.csv")))
+    .Then<string[]>(prev => {
+        return new Success<List<string[]>>(
+            prev!
+                .Select(l => l.Split(","))
+                .ToList()
+        );
+    })
+    .Then<List<string[]>>(prev => {
+        return new Success<int>(prev.Count());
+    });
+
+
+var csvCountResult = await pipeline.InvokeAsync();
+Console.WriteLine(csvCountResult.GetType().Name);
+if(csvCountResult is Success<int> success)
+    Console.WriteLine($"CSV contains {success.Value} rows");
